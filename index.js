eaf7a049936eee6528ff714c6c073b264bf68ef6
rupom <furqanrupom978@gmail.com>
2023-06-10 03:00:49 +0600

    added admin and instructor secure apis as well created three middleware for verification of admin , student's and instructor

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
 require('dotenv').config()
 app.use(morgan('dev'))
+
+
+
+
+
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "Unauthorized access denied!" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.SECRET_ACCESS_TOKEN, (error, decoded) => {
+    if (error) {
+      return res
+        .status(403)
+        .send({ error: true, message: "Unauthorized access denied!" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+
+
+
+
 
 
 
@@ -34,15 +64,58 @@ async function run() {
 
 
 
-    // TODO : we will apply get operation in this users routes when we wanna to do admin routes
+    const verifyAdmin = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user.role !== "admin") {
+        return res
+          .status(401)
+          .send({ error: true, message: "Access forbidden" });
+      }
+      next();
+    };
+
+
+    const verifyInstructor = async (req, res, next) => {
+      const email = req.decoded.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user.role !== "instructor") {
+        return res
+          .status(401)
+          .send({ error: true, message: "Access forbidden" });
+      }
+      next();
+    };
+
+
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.SECRET_ACCESS_TOKEN, {
+        expiresIn: "1hr",
+      });
+      res.send({ token });
+    });
 
 
     // users
 
-    app.get('/users',async(req,res)=>{
+    app.get('/users',verifyJWT,verifyAdmin,async(req,res)=>{
       const result = await usersCollection.find().toArray()
       res.send(result);
     })
+
+
+
+
+
+
+
+
+
+
 
     app.post('/users',async(req,res)=>{
       const users = req.body;
@@ -57,13 +130,49 @@ async function run() {
 
 
 
+    // verify admin
+
+    app.get("/users/admin/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      console.log(req.decoded.email);
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ admin: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { admin: user?.role === "admin" };
+      console.log(result);
+      res.send(result);
+    });
+
+
+
+    // verifyInstructor
+
+    app.get("/users/instructor/:email", verifyJWT, async (req, res) => {
+      const email = req.params.email;
+      console.log(email);
+      console.log(req.decoded.email);
+      if (email !== req.decoded.email) {
+        return res.status(403).send({ instructor: false });
+      }
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.role === "instructor" };
+      console.log(result);
+      res.send(result);
+    });
+
+
+
 
     // classes
 
 
     // all classes
 
-      app.get('/allClasses',async(req,res)=>{
+      app.get('/allClasses',verifyJWT,verifyAdmin,async(req,res)=>{
         const result = await classesCollection.find().toArray()
         res.send(result);
       })
@@ -71,7 +180,7 @@ async function run() {
 
     // TODO : verify users
 
-    app.get('/classes',async(req,res)=>{
+    app.get('/classes',verifyJWT,verifyInstructor,async(req,res)=>{
       const email = req.query.email;
       const query = {instructorEmail:email};
       const result = await classesCollection.find(query).toArray()
