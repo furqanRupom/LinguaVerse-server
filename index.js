3c54687ed874a8976208fe9364b5502026f78662
rupom <furqanrupom978@gmail.com>
2023-06-09 16:37:30 +0600

    create another apis classes routes for delete any specific class

--- index.js
+++ index.js
@@ -91,6 +91,14 @@ async function run() {
     })
 
 
+    app.delete('/classes/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await classesCollection.deleteOne(query)
+      res.send(result)
+    })
+
+
 
 
 
